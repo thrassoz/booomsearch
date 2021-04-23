1b68ca406c08a45118b17f58814a280127817478
Thrassos Oikonomou <thrassosoikonomou@iMac-Thrassos.lan>
2021-04-23 09:21:55 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, redirect, render_template, url_for
+import bs4
+import requests
+
+
+app = Flask(__name__)
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+
+        if request.form['name'] != '':
+            res = requests.get('https://www.booomtoys.gr/el/products-list&product_search=' + request.form['name'])
+            page = bs4.BeautifulSoup(res.text, 'html.parser')
+            results = page.select('.prodSliderCart')
+            if len(results) > 0:
+                return redirect('https://www.booomtoys.gr/el/products-list&product_search=' + request.form['name'], 301)
+            else: #decide were to redirect the client if results not found
+                return render_template('index.html', key=len(results), posted=request.form)
+
+        elif request.form['code'] != '':
+            res = requests.get('https://www.booomtoys.gr/el/products-list&product_search=' + request.form['code'])
+            page = bs4.BeautifulSoup(res.text, 'html.parser')
+            results = page.select('.prodSliderCart')
+            if len(results) > 0:
+                return redirect('https://www.booomtoys.gr/el/products-list&product_search=' + request.form['code'], 301)
+            else:
+                return render_template('index.html', key=len(results), posted=request.form)
+
+        elif request.form['company'] != '':
+            res = requests.get('https://www.booomtoys.gr/el/products-list&product_search=' + request.form['company'])
+            page = bs4.BeautifulSoup(res.text, 'html.parser')
+            results = page.select('.prodSliderCart')
+            if len(results) > 0:
+                return redirect('https://www.booomtoys.gr/el/products-list&product_search=' + request.form['company'], 301)
+            else:
+                return render_template('index.html', key=len(results), posted=request.form)
+        else:
+            return redirect('https://www.booomtoys.gr/el/' + request.form['category'], 301)
+    return render_template('index.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
